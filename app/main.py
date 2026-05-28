a70af0ea41da162926cc835da4b39b9d44fb957a
Houston Barros <houstonbarros@MacBook-Air-de-Houston.local>
2026-05-28 14:55:01 -0300

    fix(main): fixed unused import

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import uvicorn
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 
